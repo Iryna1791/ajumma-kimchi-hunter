ae5a638da534381327d0cc0bfcd7652341e27ba9
Iryna1791 <irynawebdev@gmail.com>
2022-12-05 14:24:04 +0100

    minus

--- js/game.js
+++ js/game.js
@@ -1,5 +1,10 @@
-class AjummaKimchiHunter {
-    constructor(player) {
-        this.player = player;
+class Game {
+    constructor() {
+        this.player = null;
+        this.obstacles= [];
     }
 }
+player = document.querySelector('player')
+document.addEventListener('keydown', function(event){
+    if(event.keyCode())
+})
